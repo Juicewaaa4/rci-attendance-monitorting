df0ba691cab3fb8800d8b09dd188e6b341976452
noellao25 <noel_lao25@yahoo.com>
2026-05-22 19:47:19 +0800

    Auto migrate and seed

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,10 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Automatically setup the live database (create tables and admin account)
+php artisan migrate --force
+php artisan db:seed --force
+
 # Start PHP-FPM in background
 php-fpm -D
 
